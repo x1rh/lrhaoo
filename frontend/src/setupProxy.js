6ec337bf822f042239fbd84e8e6b00acc597c148
lrhhhhhh <lrhhhhhh@gmail.com>
2020-05-10 01:14:29 +0800

    fix setupProxy.js

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,18 +1,41 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const {createProxyMiddleware} = require('http-proxy-middleware');
 
-module.exports = function (app) {
-    app.use(
-        "/api",
-        createProxyMiddleware({
-            target: "http://backend:5000/",
-            changeOrigin: true
-        })
-    );
-    app.use(
-        "/auth",
-        createProxyMiddleware({
-            target: "http://backend:5000/",
-            changeOrigin: true
-        })
-    );
-};
+console.log('what is NONE_ENV:' + process.env.NODE_ENV);
+
+if (process.env.NODE_ENV === 'production') {
+    module.exports = function (app) {
+        app.use(
+            "/api",
+            createProxyMiddleware({
+                target: "http://backend:5000/",
+                changeOrigin: true
+            })
+        );
+        app.use(
+            "/auth",
+            createProxyMiddleware({
+                target: "http://backend:5000/",
+                changeOrigin: true
+            })
+        );
+    };
+} else if(process.env.NODE_ENV === 'development'){
+    module.exports = function (app) {
+        app.use(
+            "/api",
+            createProxyMiddleware({
+                target: "http://localhost:5000/",
+                changeOrigin: true
+            })
+        );
+        app.use(
+            "/auth",
+            createProxyMiddleware({
+                target: "http://localhost:5000/",
+                changeOrigin: true
+            })
+        );
+    };
+}
+
+
