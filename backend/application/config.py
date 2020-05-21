a132eaf23acd453dec3c3d42fcae3044e73e3a08
lrhhhhhh <lrhhhhhh@gmail.com>
2020-05-22 03:11:59 +0800

    fix gunicorn setting

--- backend/application/config.py
+++ backend/application/config.py
@@ -1,6 +1,8 @@
 import os
 from datetime import timedelta
+from dotenv import load_dotenv
 
+load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY')
--- backend/application/gunicorn.conf.py
+++ backend/application/gunicorn.conf.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import multiprocessing
 import gevent.monkey
 
@@ -8,4 +9,3 @@ bind = '0.0.0.0:5000'
 workers = multiprocessing.cpu_count() * 2 + 1
 worker_class = 'gunicorn.workers.ggevent.GeventWorker'
 x_forwarded_for_header = 'X-FORWARDED-FOR'
-
